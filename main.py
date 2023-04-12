6cc501fd4565a2c4388bad3365d13b98ce0ead13
EvgeniiNoName <125493179+EvgeniiNoName@users.noreply.github.com>
2023-04-12 21:38:20 +0700

    Commit (12.04.2023)

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import datetime
 import requests
 import json
-import os
+from pprint import pprint
 from tqdm import tqdm
 
 class VKdownloader:
     def __init__(self, id: str):
         self.id = id       
 
-    def download_photos(self, id, YAtoken, count_photo):
+    def download_photos(self, id, count_photo):
         if count_photo > 0:
             count_photo = count_photo
         else:
             count_photo = 5
 
-        with open('info/VKtoken.txt', 'r') as file_object:
-            VKtoken = file_object.read().strip()
+        with open('info/token_VK.txt', 'r') as file_object:
+            token_VK = file_object.read().strip()
         url = 'https://api.vk.com/method/photos.getAll'
         params = {
             'owner_id': id,
-            'access_token': VKtoken,
+            'access_token': token_VK,
             'photo_sizes': '1',
             'extended': '1',
             'v':'5.131',
-            'count': count_photo
-            
+            'count': count_photo            
         }
         res = requests.get(url, params=params).json()['response']['items']
+        dict_photo = {}
         list_name_photo = []
         photo_information = []
-        for item in tqdm(res):
+        for item in  res:
             name_photo = item['likes']['count']
             if name_photo in list_name_photo:
                 date = datetime.date.fromtimestamp(item['date'])
@@ -41,44 +41,46 @@ class VKdownloader:
                         'file_name': str(name_photo) + '.jpg',
                         'size': size['type']
                     })
-                    YaUploader(YAtoken).upload(name_photo, size['url'])
-
+                    dict_photo[name_photo] = size['url']
         with open('info_photo.json', 'w') as f:
             pass
         with open('info_photo.json', 'a') as f:
             json.dump(photo_information, f, indent=4)
+        return dict_photo
 
 class YaUploader:
-    def __init__(self, YAtoken: str):
-        self.YAtoken = YAtoken
-        self.headers = {"Authorization": self.YAtoken}
+    def __init__(self, token_YA: str):
+        self.token_YA = token_YA
+        self.headers = {"Authorization": self.token_YA}
 
     def new_folder(self):
         url = 'https://cloud-api.yandex.net/v1/disk/resources'
         requests.put(
             url,
             headers = self.headers,
-            params={'path': 'BackUp/'}
+            params={'path': folder.split('/')[0]}
         )
-        date = datetime.datetime.now()
-        folder = 'BackUp/'+ str(id)+ '_' + str(date.strftime('%y-%m-%d %H-%M-%S'))
         requests.put(
             url,
             headers = self.headers,
             params={'path': folder}            
         )
-        return folder 
      
-    def upload(self, name_photo, url):
-        requests.post(
-            'https://cloud-api.yandex.net/v1/disk/resources/upload',
-            headers = self.headers,
-            params={'path': str(new_folder) + '/' + str(name_photo) + '.jpg', 'url': url}
-        )             
+    def upload(self, dict_photo, folder):
+        for name_photo, url_photo in tqdm(dict_photo.items()):
+            path_photo = folder + '/' + str(name_photo) + '.jpg'
+            requests.post(
+                'https://cloud-api.yandex.net/v1/disk/resources/upload',
+                headers = self.headers,
+                params={'path': path_photo, 'url': url_photo}
+            )          
 
 if __name__ == '__main__':
-    YAtoken = input("Введите токен: ")   
+    token_YA = input("Введите токен: ")   
     id = input("Введите ID пользователя: ")
     count = int(input('Введите количество фотографий (по умолчанию 5): '))
-    new_folder = YaUploader(YAtoken).new_folder()
-    downloader = VKdownloader(id).download_photos(id, YAtoken, count)
+    folder = 'BackUp/'+ str(id) + '_' + str(datetime.datetime.now().strftime('%y-%m-%d %H-%M-%S'))
+    YaUploader(token_YA).new_folder(folder)
+    downloader = VKdownloader(id).download_photos(id, count)
+    YaUploader(token_YA).upload(downloader)
+   
